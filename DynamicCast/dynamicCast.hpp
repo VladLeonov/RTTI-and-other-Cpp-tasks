b23e3c24479ebc88a5cefedbeafc6e48219c5987
Vladislav Leonov <vlad_leonov@bk.ru>
2018-11-20 22:17:35 +0300

    Adding void and null pointer processing in my_dynamic_cast method.

--- DynamicCast/dynamicCast.hpp
+++ DynamicCast/dynamicCast.hpp
@@ -2,14 +2,27 @@
 #include "dynamicCast.h"
 
 namespace dynamicCast {
+
 	template<class T1, class T2>
-	T1* my_dynamic_cast(T2*) {
-		//
+	T1* my_dynamic_cast(T2* ptr) {
+
+		if (ptr == nullptr) {
+			return (T1*) nullptr;
+		}
+		
+		VirtualTable::ClassesRelationship vtcr = VirtualTable::getRelationship(ptr->getTypeName(), typeid(T1).name());
+	}
+
+
+	template<class T2>
+	void* my_dynamic_cast(T2* ptr) {
+		VirtualTable::ClassesRelationship vtcr = VirtualTable::getRelationship(ptr->getTypeName(), typeid(T2).name());
+		return reinterpret_cast<void*>(reinterpret_cast<char*>(ptr) - vtcr.offset);
 	}
 
 
 	template<class T1, class T2>
-	T1& my_dynamic_cast(T2&) {
+	T1& my_dynamic_cast(T2& obj) throw(badCastException) {
 		//
 	}
 }
